dbdc9a58158d0e8f72197533cd85348f083e3dfd
bichusasi <bichusasi2028@cs.sjcetpalai.ac.in>
2024-10-04 16:08:39 +0530

    Add files via upload
    basics pythoin

---
+++ bichu.py
@@ -0,0 +1,81 @@
+#print("Hello World")
+#print("Hello\nWorld")
+'''if 5 > 2:
+    print("five is greater than two!")'''
+
+'''if 5>2:
+    print("five is greater than two")
+if 5 > 2:
+    print("five is greater than two")
+if 5 > 2:
+        print("five is greater than two")
+
+if 5 > 2:
+    print("five is greater than two")
+    print("five is greater than two")
+
+x = 5
+x = "Bichu"
+print(x)
+
+x = str(3)
+y = int(3)
+z = float(3)
+print(x)
+print(y)
+print(z)
+
+x = 5
+y = "Bichu"
+z = 3.7
+a = 1j
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
+
+a = 4
+A = "Bichu"
+print(a)
+print(A)
+
+x, y, z = "orange", "banana", "cherry"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "orange"
+print(x)
+print(y)
+print(z)
+
+x = "python "
+y = "is "
+z = "awesome "
+print(x + y + z)
+
+username= input("Enter username:")
+print("username is:" + username)
+
+a = "Hello "
+b = "World"
+c = a + b
+print(c)
+
+a = "Hello, World!"
+print(a[6])'''
+
+a = "Hello, World!"
+print(a[:5])
+
+
+
+
+
+
+
+
+
+
+
+
