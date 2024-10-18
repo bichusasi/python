214e53750393236a1d88973cc6ce3c989901eac2
bichusasi <bichusasi2028@cs.sjcetpalai.ac.in>
2024-10-18 16:23:13 +0530

    Add files via upload

---
+++ maths_module.py
@@ -0,0 +1,13 @@
+'''
+Author:Bichu Sasi
+Date:18-10-2024
+Discription:Write a Python program that uses functions from the math module to perform the following operations on a number provided by the user:
+
+import math
+number=int(input("Enter a number:"))
+square_root=math.sqrt(number)
+print("square root of",number,":",square_root)
+factorial=math.factorial(number)
+print("factorial of",number,":",factorial)
+power=math.pow(number,2)
+print("power of",":",power)'''
