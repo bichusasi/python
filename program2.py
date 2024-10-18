8ab365ec1134bf8d9f171c2961e7f0c8da916149
bichusasi <bichusasi2028@cs.sjcetpalai.ac.in>
2024-10-18 15:09:32 +0530

    Add files via upload

---
+++ program2.py
@@ -0,0 +1,9 @@
+'''
+Author:Bichu Sasi
+Date:18-10-2024
+discription:Write a Python program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. Finally, display the new string.
+'''
+str1="Hello, world"
+str2=str1[7:13]
+str3=" Everyone!"
+print(str2+str3)
