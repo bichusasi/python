ca40df00266c80d8a00a139a5492664a6c080b2a
bichusasi <bichusasi2028@cs.sjcetpalai.ac.in>
2024-10-25 15:42:05 +0530

    Add files via upload

---
+++ conditiona_statement_2.py
@@ -0,0 +1,13 @@
+'''
+Author:Bichu Sasi
+Darte:25/10/2024
+Discription: Write a Python program to find the psitive number,negative number and zero.
+'''
+
+number=int(input("Enter the number:"))
+if (number>0):
+    print(number,"is posistive number")
+elif (number<0):
+    print(number,"is negative number")
+else:
+    print(number,"is a zero")
