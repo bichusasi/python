141b1a3373610079efa53f70b869c7b153267049
bichusasi <bichusasi2028@cs.sjcetpalai.ac.in>
2024-11-30 16:02:21 +0530

    Add files via upload

---
+++ reverse_string.py
@@ -0,0 +1,10 @@
+'''Author : Bichu sasi
+Description : 4. Write a Python program to reverse a string.
+Sample String : "1234abcd"
+Expected Output : "dcba4321"'''
+
+def reverse (str):
+    print("reverse of string is :",str[-1::-1])
+str = input("Enter string : ")
+reverse (str)
+
