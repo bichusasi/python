837214441e6204d10663a0698ff105cb94b6934a
bichusasi <bichusasi2028@cs.sjcetpalai.ac.in>
2024-12-19 09:42:59 +0530

    Add files via upload

---
+++ dictionary_prblms.py
@@ -0,0 +1,14 @@
+"""words = ["apple","banana","apple","orange","banana","apple"]
+word_count = {}
+for word in words:
+    word_count[word] = word_count.get(word,0)+1
+print(word_count)"""
+
+"""word = input("Eneter sentence : ")
+new = word.split(" ")
+word_counts = {}
+for words in new:
+    word_counts[words] = word_counts.get(word,0)+1
+print(word_counts)"""
+
+
